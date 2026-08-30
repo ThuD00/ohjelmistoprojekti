e613a602b3221e03f76c42af406f0b51f981ba83
ThuD00 <ray_00@icloud.com>
2026-08-30 15:29:18 +0300

    Spring Boot projektin lisäys

---
+++ lipputoimisto/src/main/java/ohjelmistoprojekti/lipputoimisto/LipputoimistoApplication.java
@@ -0,0 +1,13 @@
+package ohjelmistoprojekti.lipputoimisto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LipputoimistoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LipputoimistoApplication.class, args);
+	}
+
+}
