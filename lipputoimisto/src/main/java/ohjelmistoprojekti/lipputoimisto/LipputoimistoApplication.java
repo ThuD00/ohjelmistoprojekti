12f82b50c88304f82e8d4ec1a9137c875d72e53c
Jere Ahonen <lasetjer@gmail.com>
2026-09-16 01:46:55 +0300

    Confliktion poistoa

--- lipputoimisto/src/main/java/ohjelmistoprojekti/lipputoimisto/LipputoimistoApplication.java
+++ lipputoimisto/src/main/java/ohjelmistoprojekti/lipputoimisto/LipputoimistoApplication.java
@@ -1,13 +1,10 @@
 package ohjelmistoprojekti.lipputoimisto;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class LipputoimistoApplication {
-
 
 	public static void main(String[] args) {
 		SpringApplication.run(LipputoimistoApplication.class, args);
